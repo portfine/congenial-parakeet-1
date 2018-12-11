500e2edd493caa25d918a1d4ca2ef0ed89989e2b
Paul Konstantin Gerke <no@e.mail>
2018-12-11 09:06:23 +0100

    Add title screen

--- splash_screens.js
+++ splash_screens.js
@@ -1,17 +1,42 @@
 
 PRECACHE_TEXTURE_LIST.push(["startnewround_splash", "start_new_round.png"]);
+PRECACHE_TEXTURE_LIST.push(["title_splash", "title.png"]);
+PRECACHE_TEXTURE_LIST.push(["beams", "beams.png"]);
 
 class StartNewRoundSplash {
     constructor() {
-        this.sprite = new PIXI.Sprite(PIXI.utils.TextureCache.startnewround_splash);
-        const frame = PIXI.utils.TextureCache.startnewround_splash.frame;
+        this.sprite = new PIXI.Container();
         
-        this.sprite.position.set(
-            (1920 - frame.width) / 2, (1080 - frame.height) / 2);
-        this.sprite.visible = false;
+        this.startnewFlasher = new PIXI.Sprite(PIXI.utils.TextureCache.startnewround_splash);
+        const newroundframe = PIXI.utils.TextureCache.startnewround_splash.frame;
+        
+        this.startnewFlasher.position.set((1920 - newroundframe.width) / 2, 900);
+        this.startnewFlasher.visible = false;
+        
+        this.starburstSprite = new PIXI.Container();
+        this.starburstCenteredSprite = new PIXI.Sprite(PIXI.utils.TextureCache.beams);
+        const beamsFrame = PIXI.utils.TextureCache.beams.frame;
+        this.starburstCenteredSprite.position.set(
+            -beamsFrame.width / 2, -beamsFrame.height / 2);
+        this.starburstSprite.position.set(1920 / 2, 400);
+        this.starburstSprite.scale.set(8, 5);
+        this.starburstSprite.addChild(this.starburstCenteredSprite);
+
+        this.titleSprite = new PIXI.Container();
+        const titleOffsetSprite = new PIXI.Sprite(PIXI.utils.TextureCache.title_splash);
+        const titleFrame = PIXI.utils.TextureCache.title_splash.frame;
+        titleOffsetSprite.position.set(-titleFrame.width / 2, -titleFrame.height / 2);
+        this.titleSprite.addChild(titleOffsetSprite);
+        this.titleSprite.position.set(1920 / 2, 400);
         
         this.isVisible = false;
         this.timer = 0;
+        
+        this.initTimer = -10;
+        
+        this.sprite.addChild(this.starburstSprite);
+        this.sprite.addChild(this.startnewFlasher);
+        this.sprite.addChild(this.titleSprite);
         
         app.stage.addChild(this.sprite);
     }
@@ -27,15 +52,27 @@ class StartNewRoundSplash {
         const timeDeltaT = deltaT * (1 / 60);
         this.timer += timeDeltaT;
         if (this.timer > 0.5) {
-            this.timer %= 0.5;
-            this.sprite.visible = this.isVisible && !this.sprite.visible;
+            this.timer = 0.0;
+            this.startnewFlasher.visible = !this.startnewFlasher.visible;
+        }
+        if (!this.isVisible) {
+            this.initTimer = -10;
+        } else {
+            this.initTimer += timeDeltaT;
+            if (this.initTimer > 0.75) this.initTimer = 0.75;
+            const initRatio = this.initTimer / 0.75;
+            
+            if (initRatio > 0) {
+                this.titleSprite.scale.set(initRatio, initRatio);
+                this.starburstSprite.scale.set(8 * initRatio, 5 * initRatio);
+                
+                this.starburstSprite.rotation = (this.starburstSprite.rotation + timeDeltaT) % (2 * 3.1415);
+            }
         }
     }
     
     setVisible(b) {
         this.isVisible = b;
-        if (!b) {
-            this.sprite.visible = false;
-        }
+        this.sprite.visible = b;
     }
 };
